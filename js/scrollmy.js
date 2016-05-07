3911c6cfe2b08649016fcc6ed2e992fd91c87371
Murad <zzaakkaatt@mail.ru>
2016-05-07 13:55:53 +0300

    animated.css

---
+++ js/scrollmy.js
@@ -0,0 +1,8 @@
+   //   $(window).scroll(function() {
+   //      $('.carousel-inner__zag,.carousel-inner__zag-opacity').each(function(){
+   //        if ($(this).offset().top < $(window).scrollTop()+1000) {
+   //          $(this).addClass('animated fadeInUp');
+   //        }
+   //      });
+   //    });
+   // 
